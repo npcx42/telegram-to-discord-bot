6d0ac4e41b45fd6514b88cdf9ef629d87e238c24
Extbhite <103527176+ExtbhiteEAS@users.noreply.github.com>
2023-08-04 23:20:42 +0300

    Rewrited script with aiocron sync

--- bot.py
+++ bot.py
@@ -1,18 +1,13 @@
-import discord
 from discord.ext import commands
 from bs4 import BeautifulSoup
+import aiocron
+import discord
 import requests
-import asyncio
 
-bot_token = 'put your token here'
-channel_url = 'your tg channel url'
-discord_channel_id = channel id
-discord_bot_id = your bot id
-
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
-intents.members = True
-intents.messages = True
+TOKEN_BOT = 'discord_token' # Discord bot token here
+CHANNEL_URL = 'channel_link' # You need get ONLY public channel link
+DISCORD_CHANNEL_ID = 0 # Here you need paste channel id
+DISCORD_BOT_ID = 0 # Here you need paste bot id account
 
 last_message = None
 
@@ -34,22 +29,46 @@ def get_latest_message(channel_url):
 
     return new_messages
 
-async def check_channel():
-    while True:
-        channel = bot.get_channel(discord_channel_id)
-        message = get_latest_message(channel_url)
-        await channel.send(message[0])
-        await asyncio.sleep(3600)
+class cronjobs():
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+        
+        # * * * * * */20 - 20 seconds
+        # 0 */1 * * * - 1 hour
+        @aiocron.crontab('0 */1 * * * - 1')
+        async def get_channel_post():
+            new_message = get_latest_message(CHANNEL_URL)
+            await bot.get_channel(DISCORD_CHANNEL_ID).send(f'{new_message[0]}')
+            print(f'Sended post from \'{CHANNEL_URL}\' with content: {new_message[0]}')
 
-@bot.command(name='check')
-async def _check(ctx):
-    latest_message = get_latest_message(channel_url)
-    await ctx.send(f'Latest message from Telegram channel: {latest_message[0]}')
+class tgToDiscord(commands.Bot):
+    def __init__(self, intents):
+        super().__init__(
+            command_prefix = '!',
+            help_command = None,
+            intents = intents
+        )
+
+    async def setup_hook(self) -> None:
+        cron = cronjobs(self)
+
+    async def close(self) -> None:
+        await super().close()
+
+intents = discord.Intents.all()
+intents.members = True
+intents.messages = True
+bot = tgToDiscord(intents)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    bot.loop.create_task(check_channel())
+
+@bot.command(name='check')
+async def _check(ctx: discord.Message):
+    latest_message = get_latest_message(CHANNEL_URL)
+    current_channel_id = DISCORD_CHANNEL_ID
+    await ctx.channel.send(f'Latest message from Telegram channel ({CHANNEL_URL}): {latest_message[0]}\nCurrent channed id: {current_channel_id}')
 
 @bot.event
 async def on_message(ctx: discord.Message):
@@ -58,14 +77,14 @@ async def on_message(ctx: discord.Message):
     bot = ctx.author.bot
 
     # Тут просто, скрипт проверяет условие того, является ли сообщение от бота или нет.
-    if bot and userId == discord_bot_id:
-        if channelId == discord_channel_id:
+    if bot and userId == DISCORD_BOT_ID:
+        if channelId == DISCORD_CHANNEL_ID:
             await ctx.create_thread(
                 name = "Комментарий",
                 reason = "Allowing to comment in this message"
             )
     else:
-        if channelId == discord_channel_id:
+        if channelId == DISCORD_CHANNEL_ID:
             await ctx.delete()
 
-bot.run(bot_token)
+bot.run(TOKEN_BOT)
