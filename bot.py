18e3dfe65b068ebc747cc3c45ef09e8b3d4a79b6
Extbhite <103527176+ExtbhiteEAS@users.noreply.github.com>
2023-08-05 00:11:49 +0300

    Changed something and restored check command to work

--- bot.py
+++ bot.py
@@ -9,6 +9,36 @@ CHANNEL_URL = 'channel_link' # You need get ONLY public channel link
 DISCORD_CHANNEL_ID = 0 # Here you need paste channel id
 DISCORD_BOT_ID = 0 # Here you need paste bot id account
 
+class cronjobs():
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+        
+        # * * * * * */20 - 20 seconds
+        # 0 */1 * * * - 1 hour
+        @aiocron.crontab('0 */1 * * *')
+        async def get_channel_post():
+            new_message = get_latest_message(CHANNEL_URL)
+            await bot.get_channel(DISCORD_CHANNEL_ID).send(f'{new_message[0]}')
+            print(f'Sended post from \'{CHANNEL_URL}\' with content: {new_message[0]}')
+
+class tgToDiscord(commands.Bot):
+    def __init__(self, intents):
+        super().__init__(
+            command_prefix = '!',
+            intents = intents
+        )
+
+    async def setup_hook(self) -> None:
+        cron = cronjobs(self)
+
+    async def close(self) -> None:
+        await super().close()
+
+intents = discord.Intents.all()
+bot = tgToDiscord(intents)
+intents.members = True
+intents.messages = True
+
 last_message = None
 
 def get_latest_message(channel_url):
@@ -29,46 +59,9 @@ def get_latest_message(channel_url):
 
     return new_messages
 
-class cronjobs():
-    def __init__(self, bot: commands.Bot) -> None:
-        self.bot = bot
-        
-        # * * * * * */20 - 20 seconds
-        # 0 */1 * * * - 1 hour
-        @aiocron.crontab('0 */1 * * *')
-        async def get_channel_post():
-            new_message = get_latest_message(CHANNEL_URL)
-            await bot.get_channel(DISCORD_CHANNEL_ID).send(f'{new_message[0]}')
-            print(f'Sended post from \'{CHANNEL_URL}\' with content: {new_message[0]}')
-
-class tgToDiscord(commands.Bot):
-    def __init__(self, intents):
-        super().__init__(
-            command_prefix = '!',
-            help_command = None,
-            intents = intents
-        )
-
-    async def setup_hook(self) -> None:
-        cron = cronjobs(self)
-
-    async def close(self) -> None:
-        await super().close()
-
-intents = discord.Intents.all()
-intents.members = True
-intents.messages = True
-bot = tgToDiscord(intents)
-
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-
-@bot.command(name='check')
-async def _check(ctx: discord.Message):
-    latest_message = get_latest_message(CHANNEL_URL)
-    current_channel_id = DISCORD_CHANNEL_ID
-    await ctx.channel.send(f'Latest message from Telegram channel ({CHANNEL_URL}): {latest_message[0]}\nCurrent channed id: {current_channel_id}')
 
 @bot.event
 async def on_message(ctx: discord.Message):
@@ -86,5 +79,9 @@ async def on_message(ctx: discord.Message):
     else:
         if channelId == DISCORD_CHANNEL_ID:
             await ctx.delete()
+
+    if ctx.content.startswith('!check'):
+        latest_message = get_latest_message(CHANNEL_URL)
+        await ctx.channel.send(f'Latest message from Telegram channel (<{CHANNEL_URL}>): {latest_message[0]}\nCurrent channed id: {DISCORD_CHANNEL_ID}')
 
 bot.run(TOKEN_BOT)
